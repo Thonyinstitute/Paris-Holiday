3e9f99a335eca9bef6f395ef34bf132e5aa88a61
Thonyinstitute <>
2023-04-21 11:53:05 +0000

    Add google api verify it works and pyfiglet ascii font

--- run.py
+++ run.py
@@ -2,33 +2,41 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import pyfiglet
+import gspread
+from google.oauth2.service_account import Credentials
 
-
+"""
+Google API calling, the url and the attributes
+"""
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file", 
     "https://www.googleapis.com/auth/drive"
 ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('paris_holiday')
+
+expense = SHEET.worksheet('expense')
+data = expense.get_all_values()
+
+"""
+Google API Verification and figlet print out
+"""
+print(pyfiglet.figlet_format('Welcome to Paris'))
+print(data)
 
 
-def get_holiday_document():
+
+
+
+def get_doc_data():
     """
-    Get required documents from the traveller to achive Paris holiday
+    Ask user for travel documents
     """
-    while True:
-        print("Have your passport, Ticket and hotel reservation")
-        print("You are required to have all three documents to travel to Paris")
-        
-        data_str = input("Submit your documents here:")
 
-        holiday_document = document_str.split(",")
-
-        if validate_document(holiday_document):
-            print("Documents are Valid")
-            break
-        
-        return holiday_document
-
-
-print("Paris Holiday")
+    print("Submit traveling document")
+    print("Documents should be three")
+    print("documents type are, Passport, Ticket and Cash")
